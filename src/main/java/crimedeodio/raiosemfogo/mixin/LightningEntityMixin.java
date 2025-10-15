be7de0224a620554469b1b5e9da01f47503b58f0
Alícia <alicia_cgl@disroot.org>
2025-10-15 15:18:00 -0300

    .

--- src/main/java/crimedeodio/raiosemfogo/mixin/LightningEntityMixin.java
+++ src/main/java/crimedeodio/raiosemfogo/mixin/LightningEntityMixin.java
@@ -1,20 +1,15 @@
 package crimedeodio.raiosemfogo.mixin;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.LightningEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LightningEntity.class)
 public class LightningEntityMixin {
-	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
-	private boolean preventFirePlacement(World world, BlockPos pos, BlockState state, int flags) {
-		if (state.getBlock().getTranslationKey().contains("fire")) {
-			return false;
-		}
-		return world.setBlockState(pos, state, flags);
+	@Inject(method = "spawnFire", at = @At("HEAD"), cancellable = true)
+	private void preventFire(int fireChance, CallbackInfo ci) {
+		ci.cancel();
 	}
 }
